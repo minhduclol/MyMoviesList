554734867192f756dcc6998f6afec7eb765df4e3
Duc Nguyen <minhduclol@gmail.com>
2017-06-15 23:15:23 +0700

    add layouts

---
+++ app/src/main/java/net/webbuildup/mymovieslist/mymovieslist/movie_item.java
@@ -0,0 +1,13 @@
+package net.webbuildup.mymovieslist.mymovieslist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class movie_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_item);
+    }
+}
